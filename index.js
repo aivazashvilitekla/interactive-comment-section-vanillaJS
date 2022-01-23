840134ebb379f5389f602c197abd7c76d07a89cd
aivazashvilitekla <tekla.aivazashvili@gau.edu.ge>
2022-01-23 17:51:59 +0400

    localstorage bugfix

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ import jsonObject from "./json/data.json" assert { type: "json" };
 import { displayComments } from './js/displayComments.js';
 import { eventListeners } from './js/displayComments.js';
 
-// პირველი ჩატვირთვის შემდეგ თუ ეს დაკომენტარებული არაა, ახლიდან აწერს მონაცემებს
+
 if (!localStorage.getItem("data")) {
-    debugger;
     localStorage.setItem("data", JSON.stringify(jsonObject));
 }
-
 
 let data = JSON.parse(localStorage.getItem("data"));
 
